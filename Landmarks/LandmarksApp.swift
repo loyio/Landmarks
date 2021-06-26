606622113b0e182206a862f69b9052c3fd1a4d45
Loyio <i@loyio.me>
2021-06-26 19:26:29 +0800

    add project file

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Loyio Hex on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
