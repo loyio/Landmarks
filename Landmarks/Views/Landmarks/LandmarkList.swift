4495ae26e45b7765aad903efefe68477b1c9e695
Loyio <i@loyio.me>
2021-06-26 20:06:35 +0800

    Add Handling User Input

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -8,19 +8,25 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = true
     
     var filteredLandmarks: [Landmark]{
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationView{
-            List(filteredLandmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
-                    LandmarkRow(landmark: landmark)
+            List{
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
